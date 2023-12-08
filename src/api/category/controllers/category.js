0aa4631f0c3d72a500fed6d89dd07464d9c4979c
Akshay Bobhate <120335788+Akshay-Bob@users.noreply.github.com>
2023-12-08 08:55:47 +0530

    populate fields

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -1,9 +1,48 @@
 'use strict';
-
 /**
- * category controller
+ *  [collection-name] controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::category.category', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query('api::category.category');
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy', 'updatedBy'],
+        });
 
-module.exports = createCoreController('api::category.category');
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
+
+
+
+
+// 'use strict';
+
+// /**
+//  * category controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::category.category');
+
--- src/api/products-info/controllers/products-info.js
+++ src/api/products-info/controllers/products-info.js
@@ -1,9 +1,53 @@
+
 'use strict';
-
 /**
- * products-info controller
+ *  [collection-name] controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::products-info.products-info', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query('api::products-info.products-info');
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy', 'updatedBy'],
+        });
 
-module.exports = createCoreController('api::products-info.products-info');
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
+
+
+
+
+
+
+
+
+// 'use strict';
+
+// /**
+//  * products-info controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::products-info.products-info');
+
